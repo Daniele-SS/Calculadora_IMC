e0d5057db8448fc62c3e6d066fdcbb35ad85c5fe
MATHEUS PEREIRA DE AGUIAR <25204557@dsn127m1249389.cfp127aluno.local>
2026-09-22 11:26:30 -0300

    comecando aula

---
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.calculadoraimc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.calculadoraimc.ui.theme.CalculadoraIMCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculadoraIMCTheme {
+                ImcScreen(
+                    modifier = Modifier.padding(32.dp))
+                }
+            }
+        }
+    }
+
+@Composable
+fun ImcScreen(modifier: Modifier = Modifier){
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ){
+            Column(modifier = Modifier.fillMaxWidth()) {
+                // -- Header --
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(160.dp)
+                        .background(color = colorResource(R.color.cor_app)),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        modifier = Modifier.size(80.dp)
+                            .padding(vertical = 16.dp),
+                        painter = painterResource(R.drawable.bmi),
+                        contentDescription = "IMC",
+
+                    )
+                }
+
+//                -- Formulario --
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(horizontal = 32.dp)
+                    )
+                 {
+
+                 }
+            }
+        }
+//                    -- card resultado --
+
+    }
+}
+
